eca427ae209c51532dd435881cfebbe4b39f2dbc
NataliaSchnechel <nsschnechel@restinga.ifrs.edu.br>
2017-05-11 09:22:53 -0300

    Criação da Tela de Chat de orçamento

---
+++ UFormChat.pas
@@ -0,0 +1,26 @@
+unit UFormChat;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  UFormModelo, FMX.Layouts, FMX.MultiView, FMX.Objects,
+  FMX.Controls.Presentation;
+
+type
+  TFormChat = class(TFormModelo)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FormChat: TFormChat;
+
+implementation
+
+{$R *.fmx}
+
+end.
